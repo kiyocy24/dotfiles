08c46dab909361a3f8f698172e9cf341fe9a3ddb
Kiyoshi Kanazawa <kiyocy24@gmail.com>
2024-04-21 17:29:06 +0900

    to lua (#10)

--- install.sh
+++ install.sh
@@ -25,6 +25,8 @@ brew install tmux
 
 echo 'Install neovim'
 brew install neovim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 echo 'Install starship'
 brew install starship
